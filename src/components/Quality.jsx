789ea92331f498d22df13aec10fa7f409cc7f1da
Antonyamose <antonyamose3@gmail.com>
2026-03-03 10:34:01 +0530

    chg-upd2

--- src/components/Quality.jsx
+++ src/components/Quality.jsx
@@ -8,55 +8,55 @@ import img4 from '../assets/origin.png'
 
 const Quality = () => {
   const certificates = [
-  {
-    name: "FSSAI",
-    subtitle: "Food Safety Standards",
-    logo: img2,
-  },
-  {
-    name: "APEDA",
-    subtitle: "Export Authority",
-    logo: img3,
-  },
-  {
-    name: "Phytosanitary",
-    subtitle: "Plant Health",
-    logo:img1
-  },
-  {
-    name: "Origin",
-    subtitle: "Authenticity",
-    logo: img4,
-  },
-];
+    {
+      name: "FSSAI",
+      subtitle: "Food Safety Standards",
+      logo: img2,
+    },
+    {
+      name: "APEDA",
+      subtitle: "Export Authority",
+      logo: img3,
+    },
+    {
+      name: "Phytosanitary",
+      subtitle: "Plant Health",
+      logo: img1
+    },
+    {
+      name: "Origin",
+      subtitle: "Authenticity",
+      logo: img4,
+    },
+  ];
 
   return (
     <div id='quality' className="font-serif">
-      
+
       {/* --- NEW MINIMALIST HEADER SECTION (WHITE) --- */}
       <section className="bg-white py-10 lg:py-20  overflow-hidden">
         <div className="max-w-7xl px-4 mx-auto">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 lg:gap-16 items-start">
-            
+
             {/* Left: Big Statement */}
             <div className="lg:col-span-1">
-              <motion.span 
+              <motion.span
                 initial={{ opacity: 0 }}
                 whileInView={{ opacity: 1 }}
-                  viewport={{ once: false,  amount: 0.2}}
-                 transition={{ duration: 1.5, delay: 0.2 }}
+                viewport={{ once: false, amount: 0.2 }}
+                transition={{ duration: 1.5, delay: 0.2 }}
                 className="text-[#C5A059] uppercase lg:tracking-[0.6em] text-xs lg:text-[10px] font-bold font-sans block mb-8"
               >
                 The Gold Standard
               </motion.span>
-              <motion.h2 
+              <motion.h2
                 initial={{ opacity: 0, y: 20 }}
                 whileInView={{ opacity: 1, y: 0 }}
-                  viewport={{ once: false,  amount: 0.2}}
+                viewport={{ once: false, amount: 0.2 }}
                 transition={{ duration: 1.5, delay: 0.2 }}
                 className="text-2xl md:text-5xl font-light italic text-[#0A2540] leading-[1.1]"
               >
-                Purity Is Our 
+                Purity Is Our
                 <span className="text-[#C5A059]"> Only </span> Metric.
               </motion.h2>
             </div>
@@ -75,15 +75,15 @@ const Quality = () => {
                   ))}
                 </ul>
               </div>
-              <motion.div 
+              <motion.div
                 initial={{ opacity: 0, scale: 0.95 }}
                 whileInView={{ opacity: 1, scale: 1 }}
-                  viewport={{ once: false,  amount: 0.2}}
-                  transition={{ duration: 1.5, delay: 0.2 }}
+                viewport={{ once: false, amount: 0.2 }}
+                transition={{ duration: 1.5, delay: 0.2 }}
                 className="aspect-video overflow-hidden bg-gray-100"
               >
-                <img 
-                  src="https://images.unsplash.com/photo-1536304993881-ff6e9eefa2a6?auto=format&fit=crop&q=80&w=1000" 
+                <img
+                  src="https://images.unsplash.com/photo-1536304993881-ff6e9eefa2a6?auto=format&fit=crop&q=80&w=1000"
                   className="w-full h-full object-cover grayscale brightness-90 hover:grayscale-0 transition-all duration-1000"
                   alt="Quality Lab"
                 />
@@ -98,56 +98,55 @@ const Quality = () => {
         <div className="absolute top-0 right-0 text-[20rem] font-bold text-white/[0.02] select-none translate-x-1/2 -translate-y-1/4">
           TMR
         </div>
-
         <div className="max-w-7xl mx-auto px-4 relative z-10">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-20 items-center">
             <div>
               <Award className="text-[#C5A059] w-12 h-12 mb-8" />
               <h2 className="text-2xl md:text-5xl font-light italic mb-8 leading-tight">
-                Globally Compliant. <br  className='hidden lg:block'/>Locally Sourced.
+                Globally Compliant. <br className='hidden lg:block' />Locally Sourced.
               </h2>
               <p className="text-gray-400 font-sans text-sm leading-relaxed lg:max-w-md mb-12">
                 We bridge the gap between Indian heritage and international safety standards. Every bag is accompanied by comprehensive documentation for seamless global entry.
               </p>
-              
-             <div className="grid grid-cols-2 gap-8 border-t border-white/10 pt-6 lg:pt-12">
-  {certificates.map((cert, i) => (
-    <div key={i} className="flex flex-col items-start items-start gap-3">
-      
-      {/* Logo */}
-      <img
-        src={cert.logo}
-        alt={cert.name}
-        className="h-15 w-15  object-contain "
-      />
 
-      {/* Text */}
-      <div>
-        <h4 className="text-[#C5A059] text-xs font-bold uppercase tracking-widest mb-1">
-          {cert.name}
-        </h4>
-        <p className="text-gray-500 text-[10px] uppercase font-sans tracking-widest">
-          {cert.subtitle}
-        </p>
-      </div>
+              <div className="grid grid-cols-2 gap-8 border-t border-white/10 pt-6 lg:pt-12">
+                {certificates.map((cert, i) => (
+                  <div key={i} className="flex flex-col items-start items-start gap-3">
 
-    </div>
-  ))}
-</div>
+                    {/* Logo */}
+                    <img
+                      src={cert.logo}
+                      alt={cert.name}
+                      className="h-15 w-15  object-contain "
+                    />
+
+                    {/* Text */}
+                    <div>
+                      <h4 className="text-[#C5A059] text-xs font-bold uppercase tracking-widest mb-1">
+                        {cert.name}
+                      </h4>
+                      <p className="text-gray-500 text-[10px] uppercase font-sans tracking-widest">
+                        {cert.subtitle}
+                      </p>
+                    </div>
+
+                  </div>
+                ))}
+              </div>
             </div>
 
             <div className="relative ">
               <div className="border border-[#C5A059]/30 p-4">
-                <img 
-                  src="https://images.unsplash.com/photo-1586201375761-83865001e31c?auto=format&fit=crop&q=80&w=1000" 
-                  alt="Quality Control" 
+                <img
+                  src="https://images.unsplash.com/photo-1586201375761-83865001e31c?auto=format&fit=crop&q=80&w=1000"
+                  alt="Quality Control"
                   className="w-full grayscale brightness-75 border border-[#C5A059]/20"
                 />
               </div>
               <div className="absolute lg:-bottom-10 -bottom-5 lg:-left-10 -left-3 bg-[#C5A059] rounded-md p-3 md:p-5 lg:p-10  md:block shadow-2xl">
-                 <p className="text-[#0A2540]  text-xs md:font-bold lg:uppercase lg:tracking-[0.3em] lg:leading-relaxed">
-                   24-Point <br /> Quality <br /> Checks
-                 </p>
+                <p className="text-[#0A2540]  text-xs md:font-bold lg:uppercase lg:tracking-[0.3em] lg:leading-relaxed">
+                  24-Point <br /> Quality <br /> Checks
+                </p>
               </div>
             </div>
           </div>
